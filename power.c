fce020dd497363c5326d851a1c2011b5d2cba898
sandhiyabala <47448871+sandhiyabala@users.noreply.github.com>
2019-02-11 14:50:31 +0530

    Update power.c

--- power.c
+++ power.c
@@ -2,9 +2,7 @@
 int main()
 {
     int a,b,i,power=1;
-    printf("enter the value of a:");
     scanf("%d",&a);
-    printf("enter the value of b:");
     scanf("%d",&b);
     for(i=1;i<=b;i++)
     {
